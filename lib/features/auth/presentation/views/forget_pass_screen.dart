9cb47c030daf4151ca94f2b6a980e643714522ba
Kareem Tantawi <kareem01229526319@gmail.com>
2024-07-12 18:34:09 +0300

    finish the forget password and reset password

--- lib/features/auth/presentation/views/forget_pass_screen.dart
+++ lib/features/auth/presentation/views/forget_pass_screen.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:symphony_app/core/common/animation/animation_do.dart';
+import 'package:symphony_app/core/common/widget/app_defailt_button.dart';
+import 'package:symphony_app/core/common/widget/app_text_default_button.dart';
+import 'package:symphony_app/core/common/widget/default_icon_container.dart';
+import 'package:symphony_app/core/common/widget/defulat_text_form_feild.dart';
+import 'package:symphony_app/core/extension/extension.dart';
+import 'package:symphony_app/core/routes/routes.dart';
+import 'package:symphony_app/core/theme/font/font_family_helper.dart';
+import 'package:symphony_app/core/theme/font/font_weight_helper.dart';
 import 'package:symphony_app/core/theme/image/app_image.dart';
+import 'package:symphony_app/core/utils/spacing.dart';
 import 'package:symphony_app/features/auth/presentation/refactor/app_bar_method.dart';
 
 class ForgetPassScreen extends StatelessWidget {
@@ -12,12 +22,73 @@ class ForgetPassScreen extends StatelessWidget {
         context,
         'Forget Password',
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            AppImages.forgetPassScreen,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            verticalSpace(50),
+            CustomFadeInDown(
+              duration: 600,
+              child: Center(
+                child: Image.asset(
+                  AppImages.forgetPassScreen,
+                  height: 136,
+                ),
+              ),
+            ),
+            verticalSpace(20),
+            const CustomFadeInLeft(
+              duration: 600,
+              child: Text(
+                textAlign: TextAlign.center,
+                'A password verification will be sent to your email. You should follow up on At',
+                style: TextStyle(
+                  color: Color(0xff878787),
+                  fontSize: 15,
+                  fontWeight: FontWeightHelper.regular,
+                  fontFamily: FontFamilyHelper.fontFamily1,
+                ),
+              ),
+            ),
+            verticalSpace(30),
+            CustomFadeInRight(
+              duration: 600,
+              child: Row(
+                children: [
+                  const DefaultIconContainer(
+                    icon: Icons.email_rounded,
+                  ),
+                  horizontalSpace(20),
+                  DefaultTextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    hintText: 'Enter your email',
+                    textEditingController: TextEditingController(),
+                  ),
+                ],
+              ),
+            ),
+            verticalSpace(80),
+            CustomFadeInRight(
+              duration: 600,
+              child: AppDefaultButton(
+                onTap: () {
+                  context.pushName(Routes.resetPasswordScreen);
+                },
+                title: 'Send',
+              ),
+            ),
+            verticalSpace(10),
+            AppDefaultTextButton(
+              onPressed: () {},
+              title: 'Back to Login',
+              size: 14,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/auth/presentation/views/reset_pass_screen.dart
+++ lib/features/auth/presentation/views/reset_pass_screen.dart
@@ -1,13 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:symphony_app/core/common/animation/animation_do.dart';
+import 'package:symphony_app/core/common/widget/app_defailt_button.dart';
+import 'package:symphony_app/core/common/widget/app_text_default_button.dart';
+import 'package:symphony_app/core/common/widget/default_icon_container.dart';
+import 'package:symphony_app/core/common/widget/defulat_text_form_feild.dart';
+import 'package:symphony_app/core/theme/image/app_image.dart';
+import 'package:symphony_app/core/utils/spacing.dart';
+import 'package:symphony_app/features/auth/presentation/refactor/app_bar_method.dart';
 
 class ResetPassScreen extends StatelessWidget {
   const ResetPassScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Reset password screen'),
+    return Scaffold(
+      appBar: appBarMethod(
+        context,
+        'Reset Password',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            verticalSpace(50),
+            CustomFadeInDown(
+              duration: 600,
+              child: Center(
+                child: Image.asset(
+                  AppImages.resetPassScreen,
+                  height: 136,
+                ),
+              ),
+            ),
+            verticalSpace(20),
+            // const CustomFadeInLeft(
+            //   duration: 600,
+            //   child: Text(
+            //     textAlign: TextAlign.center,
+            //     'A password verification will be sent to your email. You should follow up on At',
+            //     style: TextStyle(
+            //       color: Color(0xff878787),
+            //       fontSize: 15,
+            //       fontWeight: FontWeightHelper.regular,
+            //       fontFamily: FontFamilyHelper.fontFamily1,
+            //     ),
+            //   ),
+            // ),
+            verticalSpace(30),
+            CustomFadeInLeft(
+              duration: 600,
+              child: Row(
+                children: [
+                  const DefaultIconContainer(
+                    icon: Icons.lock,
+                  ),
+                  horizontalSpace(22),
+                  DefaultTextFormField(
+                    textEditingController: TextEditingController(),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) {
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    hintText: 'Password',
+                  ),
+                ],
+              ),
+            ),
+            verticalSpace(16),
+            CustomFadeInLeft(
+              duration: 600,
+              child: Row(
+                children: [
+                  const DefaultIconContainer(
+                    icon: Icons.lock,
+                  ),
+                  horizontalSpace(22),
+                  DefaultTextFormField(
+                    textEditingController: TextEditingController(),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) {
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    hintText: 'Re_Password',
+                  ),
+                ],
+              ),
+            ),
+            verticalSpace(80),
+            CustomFadeInRight(
+              duration: 600,
+              child: AppDefaultButton(
+                onTap: () {
+                  // context.pushName(Routes.resetPasswordScreen);
+                },
+                title: 'Reset',
+              ),
+            ),
+            verticalSpace(10),
+            AppDefaultTextButton(
+              onPressed: () {},
+              title: 'Cancel',
+              size: 14,
+            ),
+          ],
+        ),
       ),
     );
   }
